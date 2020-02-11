e8657dd923690c920830a4f2e6942f14459c7d3f
RossSingleton <ross-singleton@hotmail.co.uk>
2020-02-11 15:43:09 +0000

    Current state is it loads the data in, without labelling it accurately, forms a model, and then prints out it's predictions

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 from collections import defaultdict
 
 
-def loadData():
+def main():
     CURRENT_PATH = "data_set"
     # read each file, add that file to a list of files belonging to the same class
     data_path = CURRENT_PATH
@@ -22,5 +22,48 @@ def loadData():
     for label in data:
         print('For label ', label, ' we have ', len(data[label]), ' documents')
 
+    label2id = {'dev_set.tsv': 2, 'train_set.tsv': 1, 'trial_set.tsv': 0}
+    # print(data['dev_set.tsv'][1])
 
-loadData()
+    # we will also store all documents in a single array to use the following bit of code
+    all_documents = []
+    # we will do the same for labels (for later)
+    all_labels = []
+    # for each label in the dictionary (key)
+    for label in data:
+        # for each document in the list of documents pertaining to that label
+        for document in data[label]:
+            # add that document to the array with all documents
+            all_documents.append(document)
+            all_labels.append(label2id[label])
+
+    print(len(all_documents), type(all_documents))
+
+    # Here we start using scikit-learn!
+    # the CountVectorizer can be used to transform each document into a 'bag-of-words' representation
+    # https://en.wikipedia.org/wiki/Bag-of-words_model
+    # Each document is then represented as *presence* or *absence* of the words in our 'bag'
+    from sklearn.feature_extraction.text import CountVectorizer
+    # We don't need to use a dictionary for counting word frequency and selecting the important ones.
+    # sklearn has this (and more advanced) built-in functions!
+    vectorizer = CountVectorizer(max_features=50, stop_words='english')
+    X = vectorizer.fit_transform(all_documents)
+    print('These are our "features":', ', '.join(vectorizer.get_feature_names()))
+    print('A value of zero means that word is not in the document, one if yes')
+    print('Each value in a document array corresponds by position with the above features')
+    print(X.toarray())
+
+    # Fit a decision tree (classifier based on a set of if-else questions to eventually make an informed decision)
+    from sklearn import tree
+    clf = tree.DecisionTreeClassifier()
+    # The fit method takes two equally long arrays, one with data points (X), and one with labels (all_labels).
+    # By convention you will see the labels referred to as 'y', and data as 'X'.
+    clf = clf.fit(X, all_labels)
+
+    # Remember the type of data we are dealing with
+    for l in data:
+        for tweet in data[l]:
+            print(l, '->', tweet.strip())
+
+
+main()
